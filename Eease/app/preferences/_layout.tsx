b9f2501e63715727b4a997ec96762bb8e359bffb
Hifza <hifzabatool990812@gmail.com>
2024-06-18 10:38:59 +0200

    fixed path

--- Eease/app/preferences/_layout.tsx
+++ Eease/app/preferences/_layout.tsx
@@ -1,8 +1,7 @@
 // app/preferences/_layout.tsx
 import React from 'react';
 import { View } from 'react-native';
-import PreferencesScreen from '@/components/preference_form/PreferencesScreen';
-
+import PreferencesScreen from '../../components/preference_form/PreferencesScreen';
 export default function PreferencesLayout() {
   return (
     <View style={{ flex: 1 }}>
