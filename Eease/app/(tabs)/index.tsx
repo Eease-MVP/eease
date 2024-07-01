784f4ccc1d7245b8a01f65626823f8a7c9a61294
Giada Unger <giada.unger02@gmail.com>
2024-07-01 19:11:13 +0200

    Renamed the components files, edited the imports in the home page file and added a new component file for the home page

--- Eease/app/(tabs)/index.tsx
+++ Eease/app/(tabs)/index.tsx
@@ -1,8 +1,8 @@
 import { RootState } from '@/store/user-slice';
 import { View, SafeAreaView, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
-import Dashboard from '@/components/home_page/dashboard';
-import HomePageNav from '@/components/home_page/homePageNav';
+import Dashboard from '@/components/home_page/Dashboard';
+import HomePageNav from '@/components/home_page/HomePageNav';
 
 export default function Home() {
   const user = useSelector((state: RootState) => state.user)
