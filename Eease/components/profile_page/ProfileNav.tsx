e353612be9ea0dc722b4bf0bc24b63ddeda7a2f7
Giada Unger <giada.unger02@gmail.com>
2024-07-01 10:36:04 +0200

    Added som more styling to the navbar to elimenate the white space on top and added safe area view

--- Eease/components/profile_page/ProfileNav.tsx
+++ Eease/components/profile_page/ProfileNav.tsx
@@ -1,44 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function ProfileNav() {
     return (
-        <View style={styles.container}>
-            <View style={styles.textContainer}>
-                <Text style={styles.navText}>My profile</Text>
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.container}>
+                <View style={styles.textContainer}>
+                    <Text style={styles.navText}>My profile</Text>
+                </View>
+                <View style={styles.iconContainer}>
+                    <Ionicons name="notifications-outline" size={24} color="black" style={styles.icon} />
+                    <Ionicons name="settings-outline" size={24} color="black" style={styles.icon} />
+                </View>
             </View>
-            <View style={styles.iconContainer}>
-                <Ionicons name="notifications-outline" size={24} color="black" style={styles.icon} />
-                <Ionicons name="settings-outline" size={24} color="black" style={styles.icon} />
-            </View>
-        </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
+    safeArea: {
+        backgroundColor: "#C7C7C7", 
+    },
     container: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center", 
-        backgroundColor: "#C7C7C7",
-        width: "100%",
-        padding: 10,
-        borderRadius: 15,
-        justifyContent: "space-between"
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 20,
+        width: '100%',
     },
     textContainer: {
         flex: 1,
-        marginLeft: "10%"
+        marginLeft: '10%',
     },
     iconContainer: {
-        flexDirection: "row",
-        marginRight: "10%"
+        flexDirection: 'row',
+        marginRight: '10%',
     },
     icon: {
-        marginLeft: 10,
+        marginLeft: 15,
     },
     navText: {
-        fontSize: 20
-    }
+        fontSize: 20,
+    },
 });
