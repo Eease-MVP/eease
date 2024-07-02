c71ad8ef491f3f0ec0964b3e731fa2dbc4750520
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-07-03 01:57:11 +0200

    Switch from "regular" slice (createSlice) to RTK Query API (createApi), which fetches and persists the state.

---
+++ Eease/store/user-api.ts
@@ -0,0 +1,134 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import * as SecureStore from 'expo-secure-store'
+import {Gender, Language} from "@/constants/ProfileInfo"
+
+const ACCESS_TOKEN_KEY = "access_token"
+
+export interface User {
+    name: string
+    gender: Gender
+    birthDate: string
+    languages: Array<Language>
+    userPrefs?: {
+        ageFrom: number
+        ageTo: number
+        genders: Array<Gender>
+        placesToAvoid: Array<string>
+    }
+}
+export interface SignInResponse {
+    accessToken: string
+}
+
+export interface SignInUpRequest {
+    email: string
+    password: string
+}
+
+interface UserJson {
+    name: string
+    gender: keyof typeof Gender
+    birthDate: string
+    languages: Array<keyof typeof Language>
+    userPrefs?: {
+        ageFrom: number
+        ageTo: number
+        genders: Array<keyof typeof Gender>
+        placesToAvoid: Array<string>
+    }
+}
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://192.168.1.122:8080/api',
+    prepareHeaders: async (headers, {endpoint}) => {
+        // add token, only for those methods that require authentication
+        if (!['signUp', 'signIn'].includes(endpoint)) {
+            const token = await SecureStore.getItemAsync(ACCESS_TOKEN_KEY)
+            if (token) headers.set('authorization', `Bearer ${token}`)
+        }
+        return headers
+    },
+})
+// Define our single API slice object
+export const userApi = createApi({
+    reducerPath: 'api',
+    baseQuery: baseQuery,
+    endpoints: builder => ({
+        signUp: builder.mutation<void, SignInUpRequest>({
+            query: (credentials) => ({
+                url: '/auth/sign_up',
+                method: 'POST',
+                body: credentials,
+            }),
+        }),
+        signIn: builder.mutation<SignInResponse, SignInUpRequest>({
+            transformErrorResponse: (value) => {
+                console.log(value)
+                return value
+            },
+            query: (credentials) => ({
+                url: '/auth/sign_in',
+                method: 'POST',
+                body: credentials,
+            }),
+            async onQueryStarted(_, {queryFulfilled}) {
+                try {
+                    const {data} = await queryFulfilled
+                    await SecureStore.setItemAsync(ACCESS_TOKEN_KEY, data.accessToken)
+                    const token = await SecureStore.getItemAsync(ACCESS_TOKEN_KEY)
+                } catch (error) {
+                    console.log('Sign-in failed:', error)
+                }
+            },
+        }),
+        fetchUser: builder.query<User, void>({
+            query: () => '/user',
+            transformResponse(json: UserJson) {
+                return transformUserJsonToUser(json)
+            },
+        }),
+        updateUser: builder.mutation<User, User>({
+            query: (user) => ({
+                url: '/user',
+                method: 'POST',
+                body: transformUserToUserJson(user),
+            }),
+            transformResponse(json: UserJson) {
+                return transformUserJsonToUser(json)
+            },
+        }),
+    }),
+})
+
+const transformUserJsonToUser = (userJson: UserJson): User => {
+    const gender = Gender[userJson.gender]
+    const languages = userJson.languages.map(language => Language[language])
+    const userPrefs = userJson.userPrefs
+        ? {
+            ...userJson.userPrefs,
+            genders: userJson.userPrefs.genders.map(genderKey => Gender[genderKey]),
+        }
+        : undefined
+
+    return {...userJson, gender, languages, userPrefs}
+}
+const transformUserToUserJson = (user: User): UserJson => {
+    const gender = (Object.keys(Gender) as Array<keyof typeof Gender>).find(key => Gender[key] === user.gender) as keyof typeof Gender
+    const languages = user.languages.map(language => (Object.keys(Language) as Array<keyof typeof Language>).find(key => Language[key] === language) as keyof typeof Language)
+    const userPrefs = user.userPrefs
+        ? {
+            ...user.userPrefs,
+            genders: user.userPrefs.genders.map(gender => (Object.keys(Gender) as Array<keyof typeof Gender>).find(key => Gender[key] === gender) as keyof typeof Gender),
+        }
+        : undefined
+
+    return {...user, gender, languages, userPrefs}
+}
+
+export const {
+    useSignUpMutation,
+    useSignInMutation,
+    useFetchUserQuery,
+    useUpdateUserMutation,
+} = userApi
+
