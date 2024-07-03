37b7dbd6b014579e03179f21d5a1715b0b1e401b
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-07-04 00:34:35 +0200

    Update index.tsx

--- Eease/app/(tabs)/index.tsx
+++ Eease/app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
 import {useFetchUserQuery} from "@/store/user-api";
-import {useEffect} from "react";
 
 export default function Home() {
-    const {data: user, error: fetchError, isLoading: isFetching} = useFetchUserQuery()
+    const {data: user, error: fetchError, isLoading: isFetching, refetch} = useFetchUserQuery()
 
-    useEffect(() => console.log("user is ", user, "error is ", fetchError), [])
     return (
+        // testing
         <View style={styles.container}>
-            <Text style={styles.title}>Home</Text>
-            <Text>Hi, {user?.name}!</Text>
-            <Text>Your birthday is {user?.birthDate}!</Text>
-            <Text>More info: {JSON.stringify(user)}!</Text>
+            <Text style={styles.title}>Hi, {user?.name}!</Text>
+            <Text style={styles.title}>Your birthday is {user?.birthDate}!</Text>
+            <Text style={styles.title}>Your gender is {user?.gender}.</Text>
+            <Text style={styles.title}>You speak following languages: {user?.languages.join(", ")}!</Text>
+
+            <Text style={styles.title}>According to your preferences you'd like to talk to someone
+                who is {user?.prefs?.genders.join(", ")} and is between {user?.prefs?.ageFrom} and {user?.prefs?.ageTo} years old!</Text>
         </View>
     )
 }
@@ -19,11 +21,13 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        alignItems: 'flex-start',
         justifyContent: 'center',
+        margin: 32,
+        gap: 8,
     },
     title: {
-        fontSize: 24,
+        fontSize: 15,
         fontWeight: 'bold',
     },
 })
