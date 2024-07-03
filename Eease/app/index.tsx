c7d5956b73d1ef88d6205e0c726265e398a39faa
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-07-03 02:06:19 +0200

    Implement proper redirect.

--- Eease/app/index.tsx
+++ Eease/app/index.tsx
@@ -1,15 +1,30 @@
-import { RootState, isFilled } from "@/store/user-slice";
-import { Redirect } from "expo-router";
-import { useSelector } from "react-redux";
-
+import {ActivityIndicator} from "react-native"
+import EeaseBackground from "@/components/EeaseBackground"
+import {useFetchUserQuery} from "@/store/user-api"
+import {Redirect} from "expo-router"
 
 
 export default function RedirectScreen() {
-    const user = useSelector((state: RootState) => state.user)
+    const {error, isLoading, isSuccess, isError} = useFetchUserQuery()
 
-    if (isFilled(user)) {
-        return <Redirect href="(tabs)" />;
-    } else {
-        return <Redirect href="sign" />;
+    console.log('error is', error)
+    if (isError) {
+        if ('status' in error && error.status && typeof error.status === 'number') {
+            switch (error.status) {
+                case 403:
+                    return <Redirect href={'/sign'}/>
+                case 404: // 404 = you've logged in, but hasn't created a user yet.
+                    return <Redirect href="/sign_up/createUser"/>
+            }
+        }
+        return <Redirect href={'/sign'}/>
     }
+
+    if (isLoading) {
+        return <EeaseBackground>
+            <ActivityIndicator/>
+        </EeaseBackground>
+    }
+
+    if (isSuccess) return <Redirect href="(tabs)"/>
 }
