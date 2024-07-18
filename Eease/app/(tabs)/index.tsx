e9995491d827e26a060851e25065aa51d143f760
Giada Unger <giada.unger02@gmail.com>
2024-07-18 18:32:44 +0200

    Made the home page scrollable

--- Eease/app/(tabs)/index.tsx
+++ Eease/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { RootState } from '@/store/user-slice';
-import { View, SafeAreaView, StyleSheet } from 'react-native';
+import { View, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import Dashboard from '@/components/home_page/Dashboard';
 import HomePageNav from '@/components/home_page/HomePageNav';
@@ -10,20 +11,26 @@ export default function Home() {
   const user = useSelector((state: RootState) => state.user)
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
         <HomePageNav />
         <Dashboard />
         <ReceptorPreferencesDisplay />
         <ExploreReceptors />
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
+    backgroundColor: 'white',
+  },
+  scrollContainer: {
     alignItems: 'center',
+    paddingVertical: 20,
     gap: 40,
-    backgroundColor: "white"
-  }
-})
+    backgroundColor: 'white',
+  },
+});
